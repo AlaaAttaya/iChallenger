1ddd2163eeb5a98b09429bf06ca979973102823c
Alaa Attaya <attayaalaa@gmail.com>
2023-09-08 06:08:55 +0300

    Frontend <> Adjust Folder Structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="app"></div>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
