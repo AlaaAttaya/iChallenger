d6f2cd52ca28e38cf91a231202c59e01b5184a88
Alaa Attaya <attayaalaa@gmail.com>
2023-09-11 11:05:46 +0300

    Desktop <> Adjust Styling Of Searchbar and Avatar

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -1,12 +1,17 @@
+const base_url = "http://127.0.0.1:8000/api/";
 const burgermenu = document.getElementById("burgermenu");
 const closeleftnavbar = document.getElementById("closeleftnavbar");
 const leftnavbar = document.getElementById("leftnavbar");
 const ichallengerlogo = document.getElementById("ichallengerlogo-leftnavbar");
-const logout = document.getElementById("logout-button");
-const base_url = "http://127.0.0.1:8000/api/";
 const avatarimg = document.getElementById("avatarimg");
 const username = document.getElementById("username");
+const logout = document.getElementById("logout-button");
 const websiteButton = document.getElementById("website-button");
+const dashboardButton = document.getElementById("dashboard-button");
+const emailsButton = document.getElementById("emails-button");
+const tournamentsButton = document.getElementById("tournaments-button");
+const gamesButton = document.getElementById("games-button");
+const usersButton = document.getElementById("users-button");
 let user;
 
 function refreshToken() {
@@ -51,6 +56,11 @@ async function verifyToken() {
       user = response.data.data;
       avatarimg.src = "http://127.0.0.1:8000" + user.profileimage;
       username.innerText = user.username;
+      user_role = user.user_role_id;
+
+      if (user_role == 2) {
+        window.location.href = "../views/login.html";
+      }
     } catch (refreshError) {
       console.log(refreshError);
       window.location.href = "../views/login.html";
@@ -62,16 +72,6 @@ async function verifyToken() {
 
 window.addEventListener("load", verifyToken);
 
-window.addEventListener("resize", function () {
-  if (window.innerWidth > 400) {
-    leftnavbar.style.left = "0%";
-    ichallengerlogo.style.display = "block";
-  } else {
-    leftnavbar.style.left = "-100%";
-    ichallengerlogo.style.display = "none";
-  }
-});
-
 document.getElementById("searchnavbar").addEventListener("focus", function () {
   document.getElementById("search-icon").style.fill = "#269c55";
 });
@@ -80,15 +80,31 @@ document.getElementById("searchnavbar").addEventListener("blur", function () {
   document.getElementById("search-icon").style.fill = "#9e9e9e";
 });
 
+//Resize of Left Navbar & Searchbar
+function handleWindowResize() {
+  if (window.innerWidth > 400 && ichallengerlogo.style.display == "block") {
+    document.querySelector(".searchbar").style.marginLeft = "180px";
+  } else {
+    document.querySelector(".searchbar").style.marginLeft = "5px";
+  }
+}
+
+window.addEventListener("resize", handleWindowResize);
+
 //Burgermenu Button
 burgermenu.addEventListener("click", function () {
   leftnavbar.style.left = "0%";
   ichallengerlogo.style.display = "block";
+  if (window.innerWidth > 400) {
+    document.querySelector(".searchbar").style.marginLeft = "180px";
+  }
 });
+
 //CloseLeftNavbar Button
 closeleftnavbar.addEventListener("click", function () {
   leftnavbar.style.left = "-100%";
   ichallengerlogo.style.display = "none";
+  document.querySelector(".searchbar").style.marginLeft = "5px";
 });
 
 //Logout Button
@@ -98,7 +114,6 @@ logout.addEventListener("click", function () {
 });
 
 // Website Button
-
 websiteButton.addEventListener("click", function () {
   ipcRenderer.send("open-website-window");
 });
