c7ed2a6c79b3ff06104f62e12cd36edd1d3376ae
Alaa Attaya <attayaalaa@gmail.com>
2023-09-16 13:15:50 +0300

    Desktop <> Implement Search feature in contactus forms

--- desktop/js/users.js
+++ desktop/js/users.js
@@ -2,6 +2,8 @@ base_url = "http://127.0.0.1:8000/api/";
 const ContactusButton = document.getElementById("contactus");
 const ReportButton = document.getElementById("reportsbutton");
 const ResultsFetched = document.getElementById("fetchresults");
+const SearchResults = document.getElementById("searchusers");
+let activePage;
 document.getElementById("searchusers").addEventListener("focus", function () {
   document.getElementById("searchusers-icon").style.fill = "#269c55";
   document.getElementById("searchusersbar").style.border = "3px solid #269c55 ";
@@ -13,6 +15,11 @@ document.getElementById("searchusers").addEventListener("blur", function () {
 });
 //Contactus List
 function fetchContactUsResults(searchText) {
+  activePage = "contactus";
+  ContactusButton.style.borderBottom = "3px solid #2fd671";
+  ReportButton.style.borderBottom = "3px solid #989898";
+  ContactusButton.style.color = " #2fd671";
+  ReportButton.style.color = "#000000";
   const apiUrl = `${base_url}admin/contactus`;
 
   const requestData = {
@@ -33,6 +40,44 @@ function fetchContactUsResults(searchText) {
     .then((response) => {
       const contactUsData = response.data.data;
       ResultsFetched.innerHTML = "";
+
+      contactUsData.forEach((item) => {
+        const resultDiv = document.createElement("div");
+        resultDiv.classList.add("fetchedresults");
+
+        const nameParagraph = document.createElement("p");
+        nameParagraph.textContent = `Name: ${item.name}`;
+
+        const emailParagraph = document.createElement("p");
+        emailParagraph.textContent = `Email: ${item.email}`;
+
+        const subjectParagraph = document.createElement("p");
+        subjectParagraph.textContent = `Subject: ${item.subject}`;
+
+        const dropdownButton = document.createElement("button");
+        dropdownButton.textContent = "Show Message";
+
+        const messageParagraph = document.createElement("p");
+        messageParagraph.classList.add("message");
+        messageParagraph.style.display = "none";
+        messageParagraph.textContent = `Message: ${item.message}`;
+
+        dropdownButton.addEventListener("click", () => {
+          if (messageParagraph.style.display === "none") {
+            messageParagraph.style.display = "block";
+          } else {
+            messageParagraph.style.display = "none";
+          }
+        });
+
+        resultDiv.appendChild(nameParagraph);
+        resultDiv.appendChild(emailParagraph);
+        resultDiv.appendChild(subjectParagraph);
+        resultDiv.appendChild(dropdownButton);
+        resultDiv.appendChild(messageParagraph);
+
+        ResultsFetched.appendChild(resultDiv);
+      });
     })
     .catch((error) => {
       console.error("Error fetching contactus data:", error);
@@ -45,8 +90,18 @@ fetchContactUsResults();
 function FetchReports() {}
 ContactusButton.addEventListener("click", fetchContactUsResults);
 ReportButton.addEventListener("click", function () {
+  activePage = "reports";
   ReportButton.style.borderBottom = "3px solid #2fd671";
   ContactusButton.style.borderBottom = "3px solid #989898";
-  ReportButton.style.color = " #2fd671";
+  ReportButton.style.color = "#2fd671";
   ContactusButton.style.color = "#000000";
+});
+
+SearchResults.addEventListener("input", () => {
+  const searchText = SearchResults.value;
+
+  if (activePage === "contactus") {
+    fetchContactUsResults(searchText);
+  } else if (activePage === "reports") {
+  }
 });
