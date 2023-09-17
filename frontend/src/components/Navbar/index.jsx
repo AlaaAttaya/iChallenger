855f20955720c615d07d90bf22d56cab86486a14
Alaa Attaya <attayaalaa@gmail.com>
2023-09-17 18:04:17 +0300

    Frontend <> Burgermenu SearchBar Styling

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -6,6 +6,15 @@ import "../../styles/global.css";
 
 const Navbar = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isInputFocused, setInputFocused] = useState(false);
+
+  const handleInputFocus = () => {
+    setInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setInputFocused(false);
+  };
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
@@ -84,25 +93,170 @@ const Navbar = () => {
       </div>
 
       <div className={`navbar-burgermenu${isSidebarOpen ? " show" : ""}`}>
-        <div
-          class="close-leftnavbar"
-          id="closeleftnavbar"
-          onClick={toggleSidebar}
-        >
+        <div className="close-leftnavbar" id="closeleftnavbar">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="12"
             height="12"
             viewBox="0 0 30 30"
             fill="none"
+            onClick={toggleSidebar}
           >
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               d="M11.8371 14.9998L0 26.8371L3.16275 30L15 18.1627L26.8371 30L30 26.8371L18.1629 14.9998L29.9997 3.16279L26.8368 0L15 11.8369L3.16311 0L0.000356889 3.16279L11.8371 14.9998Z"
               fill="#2FD671"
             />
           </svg>
+        </div>
+        <div class="search-leftnavbar-wrapper">
+          <div
+            className={`search-leftnavbar ${isInputFocused ? "focused" : ""}`}
+          >
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 35 35"
+              fill="none"
+              className="search-iconsvg"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13.9589 0C6.24175 0 0 6.09868 0 13.639C0 21.1793 6.24175 27.2779 13.9589 27.2779C16.7142 27.2779 19.2641 26.4882 21.426 25.1469L31.5103 35L35 31.5903L25.044 21.8875C26.8338 19.5935 27.9179 16.751 27.9179 13.639C27.9179 6.09868 21.6761 0 13.9589 0ZM13.9589 3.20917C19.8703 3.20917 24.6334 7.86309 24.6334 13.639C24.6334 19.4148 19.8703 24.0688 13.9589 24.0688C8.04756 24.0688 3.28446 19.4148 3.28446 13.639C3.28446 7.86309 8.04756 3.20917 13.9589 3.20917Z"
+                className="search-icon"
+                id="search-icon"
+                fill="#fff"
+              />
+            </svg>
+
+            <input
+              type="text"
+              className={`searchinput ${isInputFocused ? "focused" : ""}`}
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
+              placeholder="Find Players"
+            />
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="home-dropdown">
+          <div>Home</div>{" "}
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="tournaments-dropdown">
+          <div>Tournaments</div>{" "}
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="community-dropdown">
+          <div>Community </div>
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="about-dropdown">
+          <div>About </div>
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="notifications-dropdown">
+          <div> Notifications </div>
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="messages-dropdown">
+          <div> Messages </div>
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="dropdowns-leftnavbar" id="following-dropdown">
+          <div>Following</div>
+          <div className="dropdown">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="9"
+              viewBox="0 0 14 9"
+              fill="none"
+            >
+              <path
+                d="M7 9L0 2.2496L2.33443 0L7 4.50079L11.6656 0L14 2.2496L7 9Z"
+                fill="white"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
