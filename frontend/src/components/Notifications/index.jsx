03afd3da360533f73516e316b9d48596e716f1e4
Alaa Attaya <attayaalaa@gmail.com>
2023-09-19 16:06:46 +0300

    Frontend <> Create Componenet for NotificationsDropdown

---
+++ frontend/src/components/Notifications/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const NotificationsDropdown = ({}) => {
+  return (
+    <div className="TournamentsDropdown">
+      <div>Notifications</div>
+    </div>
+  );
+};
+
+export default NotificationsDropdown;
