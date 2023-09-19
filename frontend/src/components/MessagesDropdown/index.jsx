9a14d400ef2664a87ba92cfbee3596a49a0c16a5
Alaa Attaya <attayaalaa@gmail.com>
2023-09-19 16:10:47 +0300

    Frontend <> Create Component for MessagesDropdown

---
+++ frontend/src/components/MessagesDropdown/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+const MessagesDropdown = ({ onChatgptBot, onMyMessages }) => {
+  return (
+    <div className="TournamentsDropdown">
+      <button className="dropdown-button" onClick={onChatgptBot}>
+        Chatgpt Bot
+      </button>
+      <button className="dropdown-button" onClick={onMyMessages}>
+        My Messages
+      </button>
+    </div>
+  );
+};
+
+export default MessagesDropdown;
