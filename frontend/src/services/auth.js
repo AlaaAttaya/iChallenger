f66ec818998af1b36e1211dffeabc5af4719df0b
Alaa Attaya <attayaalaa@gmail.com>
2023-09-19 09:29:45 +0300

    Frontend <> Export VerifyToken & RefreshToken Functions

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,58 @@
+import config from "./config";
+
+export const refreshToken = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return null;
+    }
+
+    const response = await fetch(`${config.base_url}/api/user/refresh`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const newToken = data.data.token;
+      console.log(newToken);
+      localStorage.setItem("token", newToken);
+      return newToken;
+    } else {
+      throw new Error("Token refresh failed");
+    }
+  } catch (error) {
+    console.error("Token refresh failed:", error);
+    throw error;
+  }
+};
+
+export const verifyToken = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return null;
+    }
+
+    const response = await fetch(`${config.base_url}/api/user/profile`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data.data);
+      return data.data;
+    } else {
+      throw new Error("Token verification failed");
+    }
+  } catch (error) {
+    console.error("Token verification failed:", error);
+    throw error;
+  }
+};
