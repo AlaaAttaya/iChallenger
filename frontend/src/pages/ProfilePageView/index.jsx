4b60eadc735a53ab9b2902d84d2a5dd76761cba8
Alaa Attaya <attayaalaa@gmail.com>
2023-09-20 08:09:14 +0300

    Frontend <> Create ProfilePageView Component

---
+++ frontend/src/pages/ProfilePageView/index.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./styles.css";
+import axios from "axios";
+import config from "../../services/config";
+import DefaultProfilePic from "../../assets/images/profilepic.png";
+import DefaultCoverPic from "../../assets/images/coverpic.png";
+const ProfilePageView = () => {
+  const [userProfileView, setUserProfileView] = useState(null);
+  return (
+    <div className="ProfilePage">
+      <div className="Profile-navbar">
+        <div className="coverPhotoContainer">
+          <img
+            src={
+              userProfileView
+                ? config.base_url + userProfileView.coverimage
+                : DefaultCoverPic
+            }
+            alt="Profile"
+            className="coverPhoto"
+          />
+        </div>
+        <div className="profileImageContainer">
+          {" "}
+          <img
+            src={
+              userProfileView
+                ? config.base_url + userProfileView.profileimage
+                : DefaultProfilePic
+            }
+            alt="Profile"
+            className="profileImage"
+          />
+        </div>
+
+        <div className="profile-navbar-buttons-container">
+          <div className="profile-navbar-buttons-wrapper">
+            <div className="user-info">
+              <span className="username-profile">Maria</span>
+              <span className="followerscount-profile">Followers Count</span>
+            </div>
+            <div className="profile-navbar-buttons buttons-middle">
+              <button>Overview</button>
+              <button>Activity</button> <button>Stream</button>
+            </div>
+            <div className="profile-navbar-buttons buttons-right">
+              <button>Follow</button>
+              <button>Message</button>
+              <button>Report</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProfilePageView;
