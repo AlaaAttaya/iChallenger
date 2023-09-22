964738bd33e2933e9cc9613f6ba066a49b9f9d63
Alaa Attaya <attayaalaa@gmail.com>
2023-09-22 16:15:44 +0300

    Frontend <> Implement Search Post in game forums

--- frontend/src/pages/GameForum/index.jsx
+++ frontend/src/pages/GameForum/index.jsx
@@ -19,10 +19,22 @@ const GameForum = ({ userProfile }) => {
   const [loading, setLoading] = useState(true);
   const [postuploaderrormessage, setPostUploadErrorMessage] = useState("");
   const [posts, setPosts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState([]);
   const navigate = useNavigate();
   const toggleUploadDivision = () => {
     setUploadVisible(!uploadVisible);
   };
+  useEffect(() => {
+    if (searchQuery.trim() === "") {
+      setFilteredPosts(posts);
+    } else {
+      const filtered = posts.filter((post) =>
+        post.description.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredPosts(filtered);
+    }
+  }, [searchQuery, posts]);
 
   const handleFileUpload = (event) => {
     setPostUploadErrorMessage("");
@@ -161,10 +173,32 @@ const GameForum = ({ userProfile }) => {
                 </svg>
               </div>
             </Link>
-            <div className="game-forum-container-navigation">
+            <div className="game-forum-container-navigation adjustor">
               <h2 className="game-forum-name">{gameforum.name}</h2>
             </div>
-            <div className="game-forum-container-navigation"></div>
+            <div className="game-forum-container-navigation">
+              <div className="postnavbarsearch">
+                <svg
+                  width="18"
+                  height="18"
+                  viewBox="0 0 35 35"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.9589 0C6.24175 0 0 6.09868 0 13.639C0 21.1793 6.24175 27.2779 13.9589 27.2779C16.7142 27.2779 19.2641 26.4882 21.426 25.1469L31.5103 35L35 31.5903L25.044 21.8875C26.8338 19.5935 27.9179 16.751 27.9179 13.639C27.9179 6.09868 21.6761 0 13.9589 0ZM13.9589 3.20917C19.8703 3.20917 24.6334 7.86309 24.6334 13.639C24.6334 19.4148 19.8703 24.0688 13.9589 24.0688C8.04756 24.0688 3.28446 19.4148 3.28446 13.639C3.28446 7.86309 8.04756 3.20917 13.9589 3.20917Z"
+                    fill="#a4a3a3"
+                  ></path>
+                </svg>
+                <input
+                  type="text"
+                  className="inputtext-search-post"
+                  placeholder="Search Posts"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </div>
+            </div>{" "}
           </div>
           {userProfile && (
             <>
@@ -207,10 +241,10 @@ const GameForum = ({ userProfile }) => {
                 </div>{" "}
               </div>
               <div className="gameforumposts-list">
-                {posts.length === 0 ? (
+                {filteredPosts.length === 0 ? (
                   <h3 style={{ color: "white" }}>No posts found.</h3>
                 ) : (
-                  posts.map((post) => (
+                  filteredPosts.map((post) => (
                     <PostCard
                       key={post.id}
                       post={post}
