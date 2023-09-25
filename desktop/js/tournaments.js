c6b648d4cd2c532d9b9214372c5358732046eca4
Alaa Attaya <attayaalaa@gmail.com>
2023-09-25 19:18:50 +0300

    Desktop <> Create The Tournament Pages

--- desktop/js/tournaments.js
+++ desktop/js/tournaments.js
@@ -1,1 +1,55 @@
 base_url = "http://127.0.0.1:8000/api/";
+
+const ListTournaments = document.getElementById("listtournaments");
+const CreateTournament = document.getElementById("createtournament");
+const ResultsFetchedTournament = document.getElementById(
+  "fetchresultstournaments"
+);
+const CreateTournamentContainer = document.getElementById(
+  "createtournamentcontainer"
+);
+const ListTournamentContainer = document.getElementById(
+  "listtournamentcontainer"
+);
+
+let activePage;
+
+document
+  .getElementById("searchtournaments")
+  .addEventListener("focus", function () {
+    document.getElementById("searchtournaments-icon").style.fill = "#269c55";
+    document.getElementById("searchtournamentsbar").style.border =
+      "3px solid #269c55 ";
+  });
+
+document
+  .getElementById("searchtournaments")
+  .addEventListener("blur", function () {
+    document.getElementById("searchtournaments-icon").style.fill = "#9e9e9e";
+    document.getElementById("searchtournamentsbar").style.border =
+      "3px solid #9e9e9e ";
+  });
+
+function ChangePage() {
+  if (activePage == "listtournaments") {
+    activePage = "createtournament";
+    CreateTournament.style.borderBottom = "3px solid #2fd671";
+    ListTournaments.style.borderBottom = "3px solid #989898";
+    CreateTournament.style.color = " #2fd671";
+    ListTournaments.style.color = "#000000";
+    ListTournamentContainer.style.display = "none";
+    CreateTournamentContainer.style.display = "flex";
+  } else {
+    activePage = "listtournaments";
+    ListTournaments.style.borderBottom = "3px solid #2fd671";
+    CreateTournament.style.borderBottom = "3px solid #989898";
+    ListTournaments.style.color = " #2fd671";
+    CreateTournament.style.color = "#000000";
+    CreateTournamentContainer.style.display = "none";
+    ListTournamentContainer.style.display = "flex";
+  }
+}
+
+ListTournaments.addEventListener("click", ChangePage);
+CreateTournament.addEventListener("click", ChangePage);
+ChangePage();
