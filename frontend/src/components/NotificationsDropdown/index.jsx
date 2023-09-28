9845422a4244b8fdbb6876850dffaed673d11a2e
Alaa Attaya <attayaalaa@gmail.com>
2023-09-28 18:41:52 +0300

    Frontend <> Handle No New Notifications

--- frontend/src/components/NotificationsDropdown/index.jsx
+++ frontend/src/components/NotificationsDropdown/index.jsx
@@ -87,61 +87,65 @@ const NotificationsDropdown = ({ userProfile }) => {
 
   return (
     <div className="NotificationsDropdown">
-      {pendingInvitations.map((invitation) => (
-        <div key={invitation.id} className="notification-item">
-          <div className="dropdown-title">
-            {invitation
-              ? `${invitation.invited_user.name} invited you to play in the tournament.`
-              : ""}
+      {pendingInvitations.length === 0 ? (
+        <div className="no-notifications">No new notifications</div>
+      ) : (
+        pendingInvitations.map((invitation) => (
+          <div key={invitation.id} className="notification-item">
+            <div className="dropdown-title">
+              {invitation
+                ? `${invitation.invited_user.name} invited you to play in the tournament.`
+                : ""}
+            </div>
+            {!accepted && !cancelled && (
+              <div className="buttons-acceptcancel">
+                <button
+                  className="dropdown-button accept-button"
+                  onClick={() => handleAccept(invitation)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2C-1.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S24.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                    <path d="M0 0h24v24H0z" fill="green" />
+                    <path d="M15.59 7.59L10 13.17 8.59 11.76 7.17 13.17 10 16l7-7z" />
+                  </svg>{" "}
+                  Accept
+                </button>
+                <button
+                  className="dropdown-button cancel-button"
+                  onClick={() => handleCancel(invitation)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2C-1.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S24.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                    <path d="M0 0h24v24H0z" fill="red" />
+                    <path d="M7 7h10v10H7z" />
+                  </svg>{" "}
+                  Cancel
+                </button>
+              </div>
+            )}
+            {accepted && (
+              <div className="message-invitation-accepted">
+                Invitation Accepted
+              </div>
+            )}
+            {cancelled && (
+              <div className="message-invitation-cancelled">
+                Invitation Cancelled
+              </div>
+            )}
           </div>
-          {!accepted && !cancelled && (
-            <div className="buttons-acceptcancel">
-              <button
-                className="dropdown-button accept-button"
-                onClick={() => handleAccept(invitation)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2C-1.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S24.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-                  <path d="M0 0h24v24H0z" fill="green" />
-                  <path d="M15.59 7.59L10 13.17 8.59 11.76 7.17 13.17 10 16l7-7z" />
-                </svg>{" "}
-                Accept
-              </button>
-              <button
-                className="dropdown-button cancel-button"
-                onClick={() => handleCancel(invitation)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2C-1.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S24.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-                  <path d="M0 0h24v24H0z" fill="red" />
-                  <path d="M7 7h10v10H7z" />
-                </svg>{" "}
-                Cancel
-              </button>
-            </div>
-          )}
-          {accepted && (
-            <div className="message-invitation-accepted">
-              Invitation Accepted
-            </div>
-          )}
-          {cancelled && (
-            <div className="message-invitation-cancelled">
-              Invitation Cancelled
-            </div>
-          )}
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
