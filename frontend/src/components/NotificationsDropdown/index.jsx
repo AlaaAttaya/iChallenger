2f8628010af5e5d1cda62cd4aa53f6c191208bb0
Alaa Attaya <attayaalaa@gmail.com>
2023-09-28 18:06:03 +0300

    Frontend <> Handle Fetching Invitations from database

--- frontend/src/components/NotificationsDropdown/index.jsx
+++ frontend/src/components/NotificationsDropdown/index.jsx
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import axios from "axios";
+import config from "../../services/config";
 
-const NotificationsDropdown = ({}) => {
+const NotificationsDropdown = ({ userProfile }) => {
+  const [accepted, setAccepted] = useState(false);
+  const [cancelled, setCancelled] = useState(false);
+  const [pendingInvitations, setPendingInvitations] = useState([]);
+
+  useEffect(() => {
+    if (userProfile) {
+      axios
+        .get(`${config.base_url}/api/user/getinvitations`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((response) => {
+          console.log("asd", response);
+          const data = response.data;
+          if (data.status === "Success") {
+            setPendingInvitations(data.data);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching pending invitations:", error);
+        });
+    }
+  }, [userProfile]);
+
   return (
     <div className="NotificationsDropdown">
-      <div className="dropdown-title">Notifications</div>
+      {pendingInvitations.map((invitation) => (
+        <div key={invitation.id} className="notification-item">
+          <div className="dropdown-title">
+            {invitation
+              ? `${invitation.invited_user.name} invited you to play in the tournament.`
+              : ""}
+          </div>
+          {!accepted && !cancelled && (
+            <div className="buttons-acceptcancel">
+              <button
+                className="dropdown-button accept-button"
+                onClick={() => handleAccept(invitation)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2C-1.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S24.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                  <path d="M0 0h24v24H0z" fill="green" />
+                  <path d="M15.59 7.59L10 13.17 8.59 11.76 7.17 13.17 10 16l7-7z" />
+                </svg>{" "}
+                Accept
+              </button>
+              <button
+                className="dropdown-button cancel-button"
+                onClick={() => handleCancel(invitation)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2C-1.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S24.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                  <path d="M0 0h24v24H0z" fill="red" />
+                  <path d="M7 7h10v10H7z" />
+                </svg>{" "}
+                Cancel
+              </button>
+            </div>
+          )}
+          {accepted && <div className="message">Invitation Accepted</div>}
+          {cancelled && <div className="message">Invitation Cancelled</div>}
+        </div>
+      ))}
     </div>
   );
 };
