b5b0e8e373486f257d0ab1b6abcca012a8dd0a6d
Alaa Attaya <attayaalaa@gmail.com>
2023-10-01 00:11:04 +0300

    Frontend <> Showcase Matches in BracketS

--- frontend/src/pages/TournamentPage/index.jsx
+++ frontend/src/pages/TournamentPage/index.jsx
@@ -107,39 +107,58 @@ const TournamentPage = ({ userProfile }) => {
             return accumulator;
           }, []);
 
-          const sortedMatches = matches.sort(
-            (a, b) => a.bracket_id - b.bracket_id || a.id - b.id
-          );
-          const bracketNumbers = {};
-          const ResultMatches = sortedMatches.map((match) => {
-            if (!(match.bracket_id in bracketNumbers)) {
-              const nextBracketNumber = Object.keys(bracketNumbers).length + 1;
-              bracketNumbers[match.bracket_id] = nextBracketNumber;
-            }
-
-            const bracketNumber = bracketNumbers[match.bracket_id];
+          const bracketStructure = data.data.brackets;
+          const ResultMatches = matches.map((match, index) => {
+            const matchId = match.id;
+            const currentBracketIndex = bracketStructure.findIndex(
+              (bracket) => bracket.id === match.bracket_id
+            );
+            const nextBracketIndex =
+              currentBracketIndex < bracketStructure.length - 1
+                ? currentBracketIndex + 1
+                : -1;
+            const nextMatchId =
+              nextBracketIndex !== -1
+                ? bracketStructure[nextBracketIndex].matches[0].id
+                : null;
 
             return {
-              id: match.id,
-              name: `Round ${bracketNumber}`,
-              nextMatchId: match.nextmatchid,
-              tournamentRoundText: `Round ${bracketNumber}`,
+              id: matchId,
+              name: `Round ${index}`,
+              nextMatchId: nextMatchId,
+              tournamentRoundText: `${index}`,
               startTime: match.match_date,
               state: match.is_completed === 1 ? "DONE" : "PENDING",
               participants: [
                 {
                   id: match.team1_id,
-                  resultText: match.winner_id === match.team1_id ? "WON" : null,
-                  isWinner: match.winner_id === match.team1_id,
+                  resultText:
+                    match.winner_id === match.team1_id &&
+                    match.team1_id !== null &&
+                    match.team1.id !== null
+                      ? "WON"
+                      : null,
+                  isWinner:
+                    match.winner_id === match.team1_id &&
+                    match.team1_id !== null &&
+                    match.team1.id !== null,
                   status: match.is_completed === 1 ? "PLAYED" : "UPCOMING",
-                  name: match.team1.name,
+                  name: match.team1 ? match.team1.name : "TBD",
                 },
                 {
                   id: match.team2_id,
-                  resultText: match.winner_id === match.team2_id ? "WON" : null,
-                  isWinner: match.winner_id === match.team2_id,
+                  resultText:
+                    match.winner_id === match.team2_id &&
+                    match.team2_id !== null &&
+                    match.team2.id !== null
+                      ? "WON"
+                      : null,
+                  isWinner:
+                    match.winner_id === match.team2_id &&
+                    match.team2_id !== null &&
+                    match.team2.id !== null,
                   status: match.is_completed === 1 ? "PLAYED" : "UPCOMING",
-                  name: match.team2.name,
+                  name: match.team2 ? match.team2.name : "TBD",
                 },
               ],
             };
@@ -149,8 +168,7 @@ const TournamentPage = ({ userProfile }) => {
           console.log("teams", teams);
           console.log("teamMembers", teamMembers);
           console.log("matches", matches);
-          console.log("dummyMatches", ResultMatches);
-          console.log("sortedMatches", sortedMatches);
+          console.log("ResultMatches", ResultMatches);
           setTournament(data.data);
           setTeams(teams);
           setTeamMembers(teamMembers);
@@ -167,7 +185,6 @@ const TournamentPage = ({ userProfile }) => {
       })
       .finally(() => {
         setLoading(false);
-        console.log("gg", bracketMatches);
       });
   }, [tournamentid]);
 
@@ -342,126 +359,7 @@ const TournamentPage = ({ userProfile }) => {
         }
       });
   };
-  const dummyMatches = [
-    {
-      id: 19753,
-      nextMatchId: null,
-      tournamentRoundText: "3",
-      startTime: "2021-05-30",
-      state: "SCHEDULED",
-      participants: [],
-    },
-    {
-      id: 19754,
-      nextMatchId: 19753,
-      tournamentRoundText: "2",
-      startTime: "2021-05-30",
-      state: "SCHEDULED",
-      participants: [
-        {
-          id: "14754a1a-932c-4992-8dec-f7f94a339960",
-          resultText: null,
-          isWinner: false,
-          status: null,
-          name: "TEST",
-          picture: "teamlogos/client_team_default_logo",
-        },
-      ],
-    },
-    {
-      id: 19755,
-      nextMatchId: 19754,
-      tournamentRoundText: "1",
-      startTime: "2021-05-30",
-      state: "SCORE_DONE",
-      participants: [
-        {
-          id: "14754a1a-932c-4992-8dec-f7f94a339960",
-          resultText: "Won",
-          isWinner: true,
-          status: "PLAYED",
-          name: "Team 1",
-          picture: "teamlogos/client_team_default_logo",
-        },
-        {
-          id: "d16315d4-7f2d-427b-ae75-63a1ae82c0a8",
-          resultText: "Lost",
-          isWinner: false,
-          status: "PLAYED",
-          name: "Team2",
-          picture: "teamlogos/client_team_default_logo",
-        },
-      ],
-    },
-    {
-      id: 19756,
-      nextMatchId: 19754,
-      tournamentRoundText: "1",
-      startTime: "2021-05-30",
-      state: "RUNNING",
-      participants: [
-        {
-          id: "d8b9f00a-0ffa-4527-8316-da701894768e",
-          resultText: null,
-          isWinner: false,
-          status: null,
-          name: "Team2",
-          picture: "teamlogos/client_team_default_logo",
-        },
-      ],
-    },
-    {
-      id: 19757,
-      nextMatchId: 19753,
-      tournamentRoundText: "2",
-      startTime: "2021-05-30",
-      state: "SCHEDULED",
-      participants: [],
-    },
-    {
-      id: 19758,
-      nextMatchId: 19757,
-      tournamentRoundText: "1",
-      startTime: "2021-05-30",
-      state: "SCHEDULED",
-      participants: [
-        {
-          id: "9397971f-4b2f-44eb-a094-722eb286c59b",
-          resultText: null,
-          isWinner: false,
-          status: null,
-          name: "Team 3",
-          picture: "teamlogos/client_team_default_logo",
-        },
-      ],
-    },
-    {
-      id: 19759,
-      nextMatchId: 19757,
-      tournamentRoundText: "1",
-      startTime: "2021-05-30",
-      state: "SCHEDULED",
-      participants: [
-        {
-          id: "42fecd89-dc83-4821-80d3-718acb50a30c",
-          resultText: null,
-          isWinner: false,
-          status: null,
-          name: "Team4",
-          picture: "teamlogos/client_team_default_logo",
-        },
-        {
-          id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
-          resultText: null,
-          isWinner: false,
-          status: null,
-          name: "Team",
-          picture: "teamlogos/r7zn4gr8eajivapvjyzd",
-        },
-      ],
-    },
-  ];
-  console.log(dummyMatches);
+
   return (
     <div className="TournamentsPageResult">
       {loading ? (
