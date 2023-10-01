b3ef166d6552e84918468038a9a4684f16c42c48
Alaa Attaya <attayaalaa@gmail.com>
2023-10-01 08:39:29 +0300

    Frontend <> Change Styling of Game CarD

--- frontend/src/components/GameCard/index.jsx
+++ frontend/src/components/GameCard/index.jsx
@@ -4,10 +4,10 @@ import "./styles.css";
 const GameCard = ({ title, image, alt }) => {
   return (
     <div className="game-card">
-      <img src={image} alt={alt} className="game-image" />
       <div className="game-name">
         <span>{title}</span>
       </div>
+      <img src={image} alt={alt} className="game-image" />
     </div>
   );
 };
