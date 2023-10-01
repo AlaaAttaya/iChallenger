7115a53a963d69c3ed075e2d53091283d94dca2a
Alaa Attaya <attayaalaa@gmail.com>
2023-10-01 12:16:01 +0300

    Frontend <> Styling Leaderboard Page

--- frontend/src/pages/Leaderboards/index.jsx
+++ frontend/src/pages/Leaderboards/index.jsx
@@ -71,20 +71,6 @@ const LeaderboardsPage = ({ userProfile }) => {
             : "navcommunity-Leaderboards-noprofile"
         }`}
       >
-        <div
-          className={` ${
-            userProfile && userProfile.leaderboard
-              ? "navcommunity-leaderboard-margin"
-              : "navcommunity-leaderboard-noprofilemargin"
-          }`}
-        >
-          <Link to="/Tournaments">
-            <button>Tournaments</button>
-          </Link>
-          <Link to="/Leaderboards">
-            <button className="thispage">Leaderboard</button>
-          </Link>
-        </div>
         {userProfile && userProfile.leaderboard && (
           <div className="user-leaderboardinfo-wrapper">
             <div className="userleaderboard-info">
@@ -124,42 +110,61 @@ const LeaderboardsPage = ({ userProfile }) => {
             </div>
           </div>
         )}
-      </div>
-      <div className="searchleaderboards-container">
-        <div
-          className={`searchgamesbar ${isInputFocused ? "focused" : ""}`}
-          id="searchgamesbar"
-        >
-          <svg
-            width="25"
-            height="34"
-            viewBox="0 0 35 35"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="searchleaderboards-iconsvg"
+        <div className="searchleaderboardbuttonswrapper">
+          <div
+            className={` ${
+              userProfile && userProfile.leaderboard
+                ? "navcommunity-leaderboard-margin"
+                : "navcommunity-leaderboard-noprofilemargin"
+            }`}
           >
-            <path
-              d="M13.9589 0C6.24175 0 0 6.09868 0 13.639C0 21.1793 6.24175 27.2779 13.9589 27.2779C16.7142 27.2779 19.2641 26.4882 21.426 25.1469L31.5103 35L35 31.5903L25.044 21.8875C26.8338 19.5935 27.9179 16.751 27.9179 13.639C27.9179 6.09868 21.6761 0 13.9589 0ZM13.9589 3.20917C19.8703 3.20917 24.6334 7.86309 24.6334 13.639C24.6334 19.4148 19.8703 24.0688 13.9589 24.0688C8.04756 24.0688 3.28446 19.4148 3.28446 13.639C3.28446 7.86309 8.04756 3.20917 13.9589 3.20917Z"
-              className={`searchleaderboards-icon ${
-                isInputFocused ? "focused" : ""
-              }`}
-            />
-          </svg>
+            <Link to="/Tournaments">
+              <button>Tournaments</button>
+            </Link>
+            <Link to="/Leaderboards">
+              <button className="thispage">Leaderboard</button>
+            </Link>
+          </div>
+          <div className="searchleaderboards-container">
+            <div
+              className={`searchgamesbar ${isInputFocused ? "focused" : ""}`}
+              id="searchgamesbar"
+            >
+              <svg
+                width="25"
+                height="34"
+                viewBox="0 0 35 35"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="searchleaderboards-iconsvg"
+              >
+                <path
+                  d="M13.9589 0C6.24175 0 0 6.09868 0 13.639C0 21.1793 6.24175 27.2779 13.9589 27.2779C16.7142 27.2779 19.2641 26.4882 21.426 25.1469L31.5103 35L35 31.5903L25.044 21.8875C26.8338 19.5935 27.9179 16.751 27.9179 13.639C27.9179 6.09868 21.6761 0 13.9589 0ZM13.9589 3.20917C19.8703 3.20917 24.6334 7.86309 24.6334 13.639C24.6334 19.4148 19.8703 24.0688 13.9589 24.0688C8.04756 24.0688 3.28446 19.4148 3.28446 13.639C3.28446 7.86309 8.04756 3.20917 13.9589 3.20917Z"
+                  className={`searchleaderboards-icon ${
+                    isInputFocused ? "focused" : ""
+                  }`}
+                />
+              </svg>
 
-          <input
-            type="text"
-            id="searchleaderboards"
-            name="searchleaderboards"
-            placeholder="Search..."
-            className={`searchgamesinput ${isInputFocused ? "focused" : ""}`}
-            onFocus={handleSearchResultsFocus}
-            onBlur={handleSearchResultsBlur}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            value={searchQuery}
-            required
-          />
+              <input
+                type="text"
+                id="searchleaderboards"
+                name="searchleaderboards"
+                placeholder="Search..."
+                className={`searchgamesinput ${
+                  isInputFocused ? "focused" : ""
+                }`}
+                onFocus={handleSearchResultsFocus}
+                onBlur={handleSearchResultsBlur}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                value={searchQuery}
+                required
+              />
+            </div>
+          </div>
         </div>
       </div>
+
       <div className="searchleaderboards-results">
         {loading ? (
           <Loading />
@@ -201,8 +206,8 @@ const LeaderboardsPage = ({ userProfile }) => {
                         {entry.points}{" "}
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
+                          width="25"
+                          height="25"
                           viewBox="0 0 36 36"
                           fill="none"
                         >
